f15c754af37fadd36dc059b06b99a85679aa5fd0
William Teder <william@cboos.com>
2013-12-22 10:03:30 -0600

    Added ability to remove server from rotation.

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
 	define("API_KEY_FILENAME","api.key");
 	define("API_KEY_LENGTH",30);
+	define("SERVERS_CONF_FILENAME","servers.conf");
 	$method = $_GET['method'];
 	$key = $_GET['apikey'];
 	function generateAPIkey(API_KEY_LENGTH) {
@@ -32,7 +33,7 @@
 		switch($_GET['method']) {
 			case "add_server":
 				if(is_string($_GET['server_id_str']) && stristr($_GET['server_id_str'],":")) {
-					$file = 'servers.conf';
+					$file = SERVER_CONF_FILENAME;
 					$current = file_get_contents($file);
 					$current .= $_GET['server_id_str']."\n";
 					file_put_contents($file, $current);
@@ -41,6 +42,24 @@
 				}
 			break;
 			case "rem_server":
+				if(is_string($_GET['server_id_str']) && stristr($_GET['server_id_str'],":")) {
+					$data = file(SERVER_CONF_FILENAME);
+					$output = array();
+					foreach($data as $line) {
+						if(trim($line) != $_GET['server_id_str']) {
+							$output[] = $line;
+						}
+					}
+					 $fp = fopen(SERVER_CONF_FILENAME, "w+");
+					 flock($fp, LOCK_EX);
+					 foreach($out as $line) {
+						 fwrite($fp, $line);
+					 }
+					 flock($fp, LOCK_UN);
+					 fclose($fp);
+				} else {
+					echo "Invalid server string argument.";
+				}
 			break;
 			case "rem_player":
 			break;
