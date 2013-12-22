ef1a2024e93e58a89a951957fff16ba8f3632c22
williamtdr <william@cboos.com>
2013-12-22 12:58:46 -0600

    Added PHP version check.

--- LoadBalancer.php
+++ LoadBalancer.php
@@ -8,6 +8,22 @@ if (0 != posix_getuid()) {
     die();
 }
 
+if (!defined('PHP_VERSION_ID')) {
+    $version = explode('.', PHP_VERSION);
+
+    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
+}
+
+if (PHP_VERSION_ID < 50207) {
+    define('PHP_MAJOR_VERSION',   $version[0]);
+    define('PHP_MINOR_VERSION',   $version[1]);
+    define('PHP_RELEASE_VERSION', $version[2]);
+}
+
+if(!(PHP_MAJOR_VERSION >= 5 && PHP_MINOR_VERSION >=4)) {
+	echo "Using PHP < 5.4.0. API will fail to run.\n"
+}
+
 exec("/sbin/sysctl net.ipv4.ip_forward=1 ; /sbin/iptables --new POCKETMINELB ; /sbin/iptables --insert INPUT --proto udp --match state --state NEW --dport 19132 -j POCKETMINELB ; /sbin/iptables --insert POCKETMINELB --jump LOG --log-prefix=\"MCPE_NEW_CONNECTION \" ; /sbin/iptables -t nat -A POSTROUTING -j MASQUERADE");
 
 $handle = popen('/usr/bin/tail -f /var/log/kern.log', 'r');
