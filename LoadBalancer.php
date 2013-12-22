084853285f237012aa3ea65cdd16fbdf329623f2
William Teder <william@cboos.com>
2013-12-22 09:15:31 -0600

    Grassroots towards reading servers from config.

--- LoadBalancer.php
+++ LoadBalancer.php
@@ -14,8 +14,26 @@ public function readAvailableServers() {
 }
 
 /* MAIN TASK */
+echo "Minecraft: Pocket Edition LoadBalancer";
+echo "by sekjun9878, williamtdr";
+echo "Reading server configuration file...";
+if(file_exists("servers.conf")) {
+	echo "Loading servers into array...";
+	$this->readAvailableServers();
+} else {
+	echo "First-time launch, creating new configuration file.";
+	echo "You should stop this program and add some servers to servers.conf.";
+	if(shell_exec("touch servers.conf") != "") {
+		echo "Failed to create configuration file, aborting.";
+		die();
+	} else {
+		echo "Created file successfully. Loading servers into array...";
+		$this->readAvailableServers();
+	}
+}
 while(true) {
     $string = fgets($handle);
+	$this->readAvailableServers();
     if(strpos($string, 'MCPE_NEW_CONNECTION') !== false) {
         preg_match_all("/SRC=.+?\..+?\..+?\..+?/", $string, $output);
         $SOURCE_IP = str_replace("SRC=", '', $output[0][0]);
